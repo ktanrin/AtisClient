b9e6c5d18fee02a290d2cd1da8900c55aca15f8e
ktanrin <ktanrin@gmail.com>
2023-11-12 21:27:21 +0700

    update ui layout

--- src/components/AtisDisplay.vue
+++ src/components/AtisDisplay.vue
@@ -23,39 +23,39 @@
       <h3 v-if="receivedData && receivedData.atisInfo"> {{ receivedData.atisInfo }} </h3>
     </div>
     <div class="optional column is-12">
-      <div class="ws" v-if="receivedData && receivedData.atisWS">
+      <div class="ws hide-on-short-windows" v-if="receivedData && receivedData.atisWS">
         <p class= "first-p" v-if="receivedData && receivedData.atisWS">WS</p>
-        <input type="text" v-if="receivedData && receivedData.atisWS" readonly class="input is-small custom-margin" :value="receivedData && receivedData.atisWS"/>
+        <input type="text" v-if="receivedData && receivedData.atisWS" readonly class="input is-warning is-small custom-margin" :value="receivedData && receivedData.atisWS"/>
       </div>
-      <div class="rcr" v-if="receivedData && receivedData.rcrContent">
+      <div class="rcr hide-on-short-windows" v-if="receivedData && receivedData.rcrContent">
         <p class= "first-p" v-if="receivedData && receivedData.rcrContent">RCR</p>
-        <textarea v-if="receivedData && receivedData.rcrContent" readonly class="textarea custom-rcr is-small custom-margin" rows="2" :value="receivedData.rcrContent"></textarea>
+        <textarea v-if="receivedData && receivedData.rcrContent" readonly class="textarea  is-warning custom-rcr is-small custom-margin" rows="2" :value="formattedRcrContent"></textarea>
       </div>
-      <div class="wind">
+      <div class="wind hide-on-short-windows">
         <p class= "first-p" v-if="receivedData && receivedData.windInfo">WIND</p>
         <input type="text" :value="receivedData && receivedData.windInfo" readonly class="input is-small custom-margin" />
       </div>     
       <div class="visibility">
         <p class= "first-p" v-if="receivedData && receivedData.visibility">VIS</p>
         <input type="text" :style="{ width: '10ch' }" :value="receivedData && receivedData.visibility" readonly class="input is-small custom-margin" />  
-        <p v-if="receivedData && receivedData.prevailWx">Prevail. Wx</p> 
-        <input type="text" :style="{ width: '10ch' }" :value="receivedData && receivedData.prevailWx" readonly class="input is-small custom-margin" />
+        <p class="hide-on-tall-windows">Prevail. Wx</p> 
+        <input type="text" :style="{ width: '10ch' }" :value="receivedData && receivedData.prevailWx" readonly class="input is-small custom-margin hide-on-tall-windows" />
       </div>
-      <div class="rvr" v-if="receivedData && receivedData.rvr && receivedData.rvr !== 'N/A'">
+      <div class="rvr hide-on-short-windows" v-if="receivedData && receivedData.rvr && receivedData.rvr !== 'N/A'">
         <p class= "first-p" v-if="receivedData && receivedData.rvr && receivedData.rvr !== 'N/A'">RVR</p>
         <input type="text" v-if="receivedData && receivedData.rvr && receivedData.rvr !== 'N/A'" readonly class="input is-small custom-margin" :value="receivedData && receivedData.rvr"/> 
       </div>
-      <div class="wx">
-        <p class= "first-p" v-if="receivedData && receivedData.weather && receivedData.weather !== 'N/A'">WX</p>
-        <input type="text" :style="{ width: '10ch' }" v-if="receivedData && receivedData.weather && receivedData.weather !== 'N/A'" readonly class="input is-small custom-margin" :value="receivedData && receivedData.weather"/> 
+      <div class="wx hide-on-short-windows">
+        <p class="first-p">WX</p>
+        <input type="text" readonly class="input is-small custom-margin" :value="formattedWeather"/> 
         <p class= "first-p" v-if="receivedData && receivedData.clouds">CLD</p>
         <input type="text" :value="receivedData && receivedData.clouds" readonly class="input is-small custom-margin" />                                                                              
       </div>
-      <div class="temp">
-        <p class= "first-p" v-if="receivedData && receivedData.temperature">TEMP</p>
-        <input type="text" :style="{ width: '10ch' }" :value="receivedData && receivedData.temperature" readonly class="input is-small custom-margin" />
-        <p v-if="receivedData && receivedData.dewPoint">DP</p>
-        <input type="text" :style="{ width: '10ch' }" :value="receivedData && receivedData.dewPoint" readonly class="input is-small custom-margin" />
+      <div class="temp hide-on-short-windows">
+        <p class= "first-p" v-if="receivedData && receivedData.temperature">T/DP</p>
+        <input type="text" :style="{ width: '15ch' }" :value="formattedTemp" readonly class="input is-small custom-margin" />
+        <p>Prevail. Wx</p>
+        <input type="text" :style="{ width: '10ch' }" :value="receivedData && receivedData.prevailWx" readonly class="input is-small custom-margin" />
       </div>       
     </div>
     <div class="qnh tile is-6 box">
@@ -65,7 +65,11 @@
     <div class="mmHg tile is-6 box">
       <h3 v-if="receivedData && receivedData.mmHg"> {{ receivedData.mmHg }} </h3>
     </div>
-    <div class="remark column is-12">
+    <div class="supplementary column is-12 hide-on-short-windows">
+      <p v-if="receivedData && receivedData.sup">Supplementary Information</p>
+      <textarea v-if="receivedData && receivedData.sup" class="textarea is-small" rows="2" :value="receivedData.sup" ></textarea>
+    </div>
+    <div class="remark column is-12 ">
       <p v-if="receivedData && receivedData.rmk">Remark</p>
       <textarea v-if="receivedData && receivedData.rmk" class="textarea is-small" rows="2" :value="receivedData.rmk" ></textarea>
     </div>
@@ -75,8 +79,11 @@
   <script>
 
 import io from 'socket.io-client';        
+//desktop as server
+//const socket = io('http://192.168.1.100:3000');
 
-const socket = io('http://192.168.1.100:3000');
+//laptop as server
+const socket = io('http://192.168.1.63:3000');
 
   export default {
     data() {
@@ -96,18 +103,40 @@ const socket = io('http://192.168.1.100:3000');
     },
     
     computed: {
-    firstHalfOfRcr() {
+      formattedRcrContent() {
       if (!this.receivedData.rcrContent) return '';
-      const halfIndex = Math.ceil(this.receivedData.rcrContent.length / 2);
-      console.log(this.receivedData.rcrContent.substring(0, halfIndex));
-      return this.receivedData.rcrContent.substring(0, halfIndex);
+
+      // Split the content into lines
+      const lines = this.receivedData.rcrContent.split('\r\n');
+      
+      // Process each line to extract and format the desired parts
+      const formattedLines = lines.map((line) => {
+        // Extract the time, runway, and condition parts using a regular expression
+        const match = line.match(/(\d{4})(\d{2})(\d{2}) (\w+) (.+?) (.+?) (.+?) (.+?)\r?$/);
+      if (match) {
+        // Ensure the time is four digits, pad with '0' if necessary
+        let [, , hour, minute, runway, firstCondition] = match;
+        hour = hour.padStart(2, '0'); // Pad hours to ensure two digits
+        minute = minute.padStart(2, '0'); // Pad minutes to ensure two digits
+
+        return `${hour}${minute}z ${runway} ${firstCondition}`;
+      }
+        return ''; // Return an empty string if the line does not match the pattern
+      });
+
+      // Join the formatted lines with a new line character
+      return formattedLines.filter(line => line).join('\n'); // Filter out any empty strings and join the rest with new lines
     },
-    secondHalfOfRcr() {
-      if (!this.receivedData.rcrContent) return '';
-      const halfIndex = Math.ceil(this.receivedData.rcrContent.length / 2);
-      console.log(this.receivedData.rcrContent.substring(halfIndex));
-      return this.receivedData.rcrContent.substring(halfIndex);
+
+      formattedTemp(){
+        const temp = this.receivedData.temperature ? `T${this.receivedData.temperature.replace('C', '')}` : '';
+        const dewPoint = this.receivedData.dewPoint ? `DP${this.receivedData.dewPoint.replace('C', '')}` : '';
+        return `${temp} ${dewPoint}`.trim();
     },
+    formattedWeather() {
+    return this.receivedData.weather === 'N/A' ? '' : this.receivedData.weather;
+  }
+
   },
 
     methods: {
@@ -155,6 +184,8 @@ const socket = io('http://192.168.1.100:3000');
     font-weight: bold;
     margin-bottom: 0 !important;
     border: solid 1px black;
+    padding-top: 0 !important;
+    padding-bottom: 0 !important;
   }
   h3, .tag{
     font-weight: bold !important;
@@ -167,7 +198,10 @@ const socket = io('http://192.168.1.100:3000');
     margin-bottom: 5px;
   }
   .first-p{
-    width: 5ch;
+    text-align: center;
+    
+    min-width: 5ch !important;
+    width: 5ch !important;
     
   }
   
@@ -184,10 +218,11 @@ const socket = io('http://192.168.1.100:3000');
     padding-bottom: 5px;
   }
   .optional{
-    text-align: left;
+    
     padding-top: 0 !important;
     padding-left: 0 !important;
     margin-top: 5px;
+    
   }
   .ws{
     display: flex;
@@ -196,6 +231,10 @@ const socket = io('http://192.168.1.100:3000');
   .rcr{
     display: flex;
     padding-bottom: 5px;
+  }
+  .input.is-warning, textarea.is-warning{
+    background-color:lightgoldenrodyellow;
+    
   }
   .custom-rcr{
     min-width: 0 !important;
@@ -227,6 +266,20 @@ const socket = io('http://192.168.1.100:3000');
   .remark{
     text-align: left;
   }
+  .supplementary{
+    text-align: left;
+  }
+  @media screen and (max-height: 450px) {
+  .hide-on-short-windows {
+    display: none !important;
+    }
+  }
+  @media screen and (min-height: 451px) {
+  .hide-on-tall-windows {
+    display: none !important;
+    }
+  }
+
   html,body,header,footer{
   background-color: lightskyblue !important;
   }
